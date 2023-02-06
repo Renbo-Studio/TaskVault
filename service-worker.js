fbfa177c74a994bf99cb4c9b377ec712b16833b4
Joshua Umahi <98918202+Renbo-Studio@users.noreply.github.com>
2023-02-06 11:48:04 +0000

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,29 +1,26 @@
 const cacheName = 'v1';
+const cacheFiles = [
+  '/',
+  'https://use.fontawesome.com/releases/v5.15.1/css/all.css'
+];
 
 self.addEventListener('install', async event => {
   console.log('Service Worker: Installed');
-  
+
   const cache = await caches.open(cacheName);
   console.log('Service Worker: Caching Files');
-  await cache.addAll([
-    '/',
-    '/index.html',
-    '/Includes/css/row.css',
-    '/Includes/css/style.css',
-    '/Includes/images',
-    '/Includes/js/app.js'
-  ]);
+  await cache.addAll(cacheFiles);
 });
 
 self.addEventListener('fetch', async event => {
   console.log('Service Worker: Fetching');
-  
+
   const response = await caches.match(event.request);
   if (response) {
     console.log('Service Worker: Found in Cache', event.request.url);
     return response;
   }
-  
+
   console.log('Service Worker: Not Found in Cache', event.request.url);
   return fetch(event.request);
 });
